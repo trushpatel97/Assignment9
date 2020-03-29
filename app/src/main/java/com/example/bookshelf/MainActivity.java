a570cc9285503472eb8495e222a675e11bd21a4a
karl <karl@macc.io>
2020-03-28 21:03:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bookshelf/MainActivity.java
@@ -0,0 +1,165 @@
+package com.example.bookshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements BookListFragment.BookSelectedInterface {
+
+    private static final String BOOKS_KEY = "books";
+    private static final String SELECTED_BOOK_KEY = "selectedBook";
+
+    FragmentManager fm;
+
+    boolean twoPane;
+    BookListFragment bookListFragment;
+    BookDetailsFragment bookDetailsFragment;
+
+    ArrayList<Book> books;
+    RequestQueue requestQueue;
+    Book selectedBook;
+
+    EditText searchEditText;
+
+    private final String SEARCH_API = "https://kamorris.com/lab/abp/booksearch.php?search=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchEditText = findViewById(R.id.searchEditText);
+
+        findViewById(R.id.searchButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchBooks(searchEditText.getText().toString());
+            }
+        });
+
+        if (savedInstanceState != null) {
+            books = savedInstanceState.getParcelableArrayList(BOOKS_KEY);
+            selectedBook = savedInstanceState.getParcelable(SELECTED_BOOK_KEY);
+        }
+        else
+            books = new ArrayList<Book>();
+
+        twoPane = findViewById(R.id.container2) != null;
+        fm = getSupportFragmentManager();
+        requestQueue = Volley.newRequestQueue(this);
+
+        bookListFragment = BookListFragment.newInstance(books);
+
+        fm.beginTransaction()
+                .replace(R.id.container1, bookListFragment)
+        .commit();
+
+        /*
+        If we have two containers available, load a single instance
+        of BookDetailsFragment to display all selected books
+         */
+        if (twoPane) {
+            if (selectedBook != null)
+                bookDetailsFragment = BookDetailsFragment.newInstance(selectedBook);
+            else
+                bookDetailsFragment = new BookDetailsFragment();
+
+            fm.beginTransaction()
+                    .replace(R.id.container2, bookDetailsFragment)
+                    .commit();
+        } else {
+            if (selectedBook != null) {
+                fm.beginTransaction()
+                        .replace(R.id.container1, BookDetailsFragment.newInstance(selectedBook))
+                        // Transaction is reversible
+                        .addToBackStack(null)
+                        .commit();
+            }
+        }
+    }
+
+    /*
+    Fetch a set of "books" from from the web service API
+     */
+    private void fetchBooks(String searchString) {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(SEARCH_API + searchString, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (response.length() > 0) {
+                    books.clear();
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject bookJSON;
+                            bookJSON = response.getJSONObject(i);
+                            books.add(new Book (bookJSON.getInt(Book.JSON_ID),
+                                    bookJSON.getString(Book.JSON_TITLE),
+                                    bookJSON.getString(Book.JSON_AUTHOR),
+                                    bookJSON.getString(Book.JSON_COVER_URL)));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    updateBooksDisplay();
+                } else {
+                    Toast.makeText(MainActivity.this, getString(R.string.search_error_message), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+    };
+
+    private void updateBooksDisplay() {
+        if (fm.findFragmentById(R.id.container1) instanceof BookDetailsFragment)
+            fm.popBackStack();
+        bookListFragment.updateBooksDisplay(books);
+    }
+
+    @Override
+    public void bookSelected(int index) {
+        selectedBook = books.get(index);
+        if (twoPane)
+            /*
+            Display selected book using previously attached fragment
+             */
+            bookDetailsFragment.displayBook(selectedBook);
+        else {
+            /*
+            Display book using new fragment
+             */
+            fm.beginTransaction()
+                    .replace(R.id.container1, BookDetailsFragment.newInstance(selectedBook))
+                    // Transaction is reversible
+                    .addToBackStack(null)
+                    .commit();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList(BOOKS_KEY, books);
+        outState.putParcelable(SELECTED_BOOK_KEY, selectedBook);
+    }
+}
